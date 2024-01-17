dcdaf24c4dbfbbcf619e7f010407307821bc9a3e
₥ia <mia.shopgal@gmail.com>
2024-01-17 17:38:09 +0800

    LinkedList for push, append, insert, pop and removeLast

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,84 @@
-import Cocoa
+example(of: "creating nodes") {
+    let node1 = Node(value:1)
+    let node2 = Node(value:2)
+    let node3 = Node(value:3)
 
-var greeting = "Hello, playground"
+    node1.next = node2
+    node2.next = node3
+
+    print(node1)
+}
+
+example(of: "push") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+
+    print(list)
+}
+
+example(of: "append") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    print(list)
+}
+
+example(of: "inserting at a point") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+
+    print("before inserting: \(list)")
+
+    var middle = list.node(at: 1)!
+    for _ in 1...4 {
+
+        middle = list.insert(-1, after: middle)
+
+    }
+
+    print("after inserting: \(list)")
+    print("inserting tail")
+    list.insert(3, after: list.tail!)
+    print("after inserting tail : \(list)")
+    print("inserting tail")
+    list.insert(3, after: list.tail!)
+    print("after inserting: \(list)")
+
+}
+
+example(of: "pop") {
+
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("before popping : \(list)")
+    let popped = list.pop()
+    print("after popping : \(list)")
+    let popped2 = list.pop()
+    print("after popping : \(list)")
+    let popped3 = list.pop()
+    print("after popping : \(list)")
+    print("popped " + String(String(describing: popped)))
+    print("popped2 " + String(String(describing: popped2)))
+    print("popped3 " + String(String(describing: popped3)))
+
+}
+
+
+example(of: "removing last") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("before removing last : \(list)")
+    let removed = list.removeLast()
+    print("after removing last : \(list)")
+    print("removed : "+String(String(describing: removed)))
+
+}
---
+++ MyPlayground.playground/Sources/LinkedList.swift
@@ -0,0 +1,119 @@
+public struct LinkedList<Value> {
+
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+
+    public init() {
+    }
+
+    public var isEmpty: Bool {
+        head == nil
+    }
+
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+
+    public mutating func append(_ value: Value) {
+
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+
+        debugPrint("appending \(value)")
+
+        debugPrint("before tails's next is \(String(describing: tail!.next))")
+        tail!.next = Node(value: value)
+
+        debugPrint("after tails's next is \(tail!.next)")
+        debugPrint("before tails is \(tail!)")
+        tail = tail!.next
+        debugPrint("after tails is \(tail!)")
+
+    }
+
+    public mutating func node(at index: Int) -> Node<Value>? {
+
+        var currentNode = head
+        var currentIndex = 0
+
+        while currentNode != nil && currentIndex < index {
+
+            currentNode = currentNode!.next
+            currentIndex += 1
+
+        }
+
+        return currentNode
+
+    }
+
+//    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+
+        guard tail !== node else {
+
+            debugPrint("appending in insert")
+            append(value)
+            return tail!
+        }
+
+        node.next = Node(value: value, next: node.next)
+
+        debugPrint("returning insert node \(String(describing: node.next))")
+
+        return node.next!
+
+    }
+
+    public mutating func pop() -> Value? {
+        defer {
+            debugPrint("starting in defer")
+            head = head?.next
+            if isEmpty {
+                debugPrint("empty tail")
+                tail = nil
+            }
+        }
+        debugPrint("end of defer")
+        return head?.value
+    }
+
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        guard head.next != nil else {
+            return pop()
+        }
+        var previous = head
+        var current = head
+        while let next = current.next {
+            debugPrint("next is \(next)")
+            debugPrint("before swapping previous \(previous), current :\(current)")
+            previous = current
+            current = next
+            debugPrint("after swapping previous \(previous), current :\(current)")
+        }
+        previous.next = nil
+        tail = previous
+        return current.value
+    }
+
+    
+    
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
